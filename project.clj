49e721cec736e54ce9a49a79285f020fd17ea320
Dima Sabanin <sdmitry@gmail.com>
2013-10-08 16:09:53 -0400

    bumped version to 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject resque-worker "1.0.0"
+(defproject resque-worker "1.0.1"
   :description "Simple resque worker with logging, statsd and graceful shutdown support."
   :url "https://github.com/dsabanin/resque-worker"
   :license {:name "Eclipse Public License"

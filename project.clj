9b045e5cbaae35231240eb0f4ed8c9ee08926471
Dima Sabanin <sdmitry@gmail.com>
2013-10-08 12:08:57 -0400

    added author and URL to project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject resque-worker "0.1.0-SNAPSHOT"
   :description "Simple resque worker with logging, statsd and graceful shutdown support."
+  :url "https://github.com/dsabanin/resque-worker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :author "Dima Sabanin <sdmitry@gmail.com>"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [dsabanin/resque-clojure "0.3.4"]
                  [wildbit/simplelog "1.0.13"]
